05d4c178e72313863d1c7c65c59896b9d90960d6
Slawek <yarpiin@gmail.com>
2016-12-18 21:35:43 +0000

    add Lionfish gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -512,6 +512,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
