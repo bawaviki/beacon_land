ed044401feb7ebe3b1ba1a16e6f2612820e3e805
Greg Kroah-Hartman <gregkh@google.com>
2017-04-03 14:54:56 +0200

    Merge 3.18.39
    Changes in 3.18.39:
    	Revert "MIPS: Reserve nosave data for hibernation"
    	Revert "sparc64: Fix numa node distance initialization"
    	netfilter: x_tables: speed up jump target validation
    	ARM: mvebu: fix HW I/O coherency related deadlocks
    	Input: xpad - validate USB endpoint count during probe
    	drm/ttm: Make ttm_bo_mem_compat available
    	ovl: handle ATTR_KILL*
    	ovl: Copy up underlying inode's ->i_mode to overlay inode
    	ALSA: hda/realtek - add new pin definition in alc225 pin quirk table
    	ALSA: ctl: Stop notification after disconnection
    	net: mvneta: set real interrupt per packet for tx_done
    	x86/quirks: Add early quirk to reset Apple AirPort card
    	posix_cpu_timer: Exit early when process has been reaped
    	ALSA: hda - fix use-after-free after module unload
    	ALSA: hda: add new AMD PCI IDs with proper driver caps
    	ALSA: hda: add AMD Stoney PCI ID with proper driver caps
    	media: fix airspy usb probe error path
    	mmc: block: fix packed command header endianness
    	tty/vt/keyboard: fix OOB access in do_compute_shiftstate()
    	ovl: verify upper dentry in ovl_remove_and_whiteout()
    	libceph: set 'exists' flag for newly up osd
    	libceph: apply new_state before new_up_client on incrementals
    	radix-tree: fix radix_tree_iter_retry() for tagged iterators.
    	pps: do not crash when failed to register
    	x86/quirks: Apply nvidia_bugs quirk only on root bus
    	x86/quirks: Reintroduce scanning of secondary buses
    	Linux 3.18.39
    
    Change-Id: Idd60191a5c5e0adcbf0dc69f617ba0843d5c08da
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- net/ipv4/netfilter/arp_tables.c
+++ net/ipv4/netfilter/arp_tables.c
@@ -379,7 +379,8 @@ static bool find_jump_target(const struct xt_table_info *t,
  * there are loops.  Puts hook bitmask in comefrom.
  */
 static int mark_source_chains(const struct xt_table_info *newinfo,
-			      unsigned int valid_hooks, void *entry0)
+			      unsigned int valid_hooks, void *entry0,
+			      unsigned int *offsets)
 {
 	unsigned int hook;
 
@@ -468,10 +469,11 @@ static int mark_source_chains(const struct xt_table_info *newinfo,
 					/* This a jump; chase it. */
 					duprintf("Jump rule %u -> %u\n",
 						 pos, newpos);
+					if (!xt_find_jump_offset(offsets, newpos,
+								 newinfo->number))
+						return 0;
 					e = (struct arpt_entry *)
 						(entry0 + newpos);
-					if (!find_jump_target(newinfo, e))
-						return 0;
 				} else {
 					/* ... this is a fallthru */
 					newpos = pos + e->next_offset;
@@ -631,6 +633,7 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
                            const struct arpt_replace *repl)
 {
 	struct arpt_entry *iter;
+	unsigned int *offsets;
 	unsigned int i;
 	int ret = 0;
 
@@ -644,8 +647,10 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
 	}
 
 	duprintf("translate_table: size %u\n", newinfo->size);
+	offsets = xt_alloc_entry_offsets(newinfo->number);
+	if (!offsets)
+		return -ENOMEM;
 	i = 0;
-
 	/* Walk through entries, checking offsets. */
 	xt_entry_foreach(iter, entry0, newinfo->size) {
 		ret = check_entry_size_and_hooks(iter, newinfo, entry0,
@@ -654,7 +659,9 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
 						 repl->underflow,
 						 repl->valid_hooks);
 		if (ret != 0)
-			break;
+			goto out_free;
+		if (i < repl->num_entries)
+			offsets[i] = (void *)iter - entry0;
 		++i;
 		if (strcmp(arpt_get_target(iter)->u.user.name,
 		    XT_ERROR_TARGET) == 0)
@@ -662,12 +669,13 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
 	}
 	duprintf("translate_table: ARPT_ENTRY_ITERATE gives %d\n", ret);
 	if (ret != 0)
-		return ret;
+		goto out_free;
 
+	ret = -EINVAL;
 	if (i != repl->num_entries) {
 		duprintf("translate_table: %u not %u entries\n",
 			 i, repl->num_entries);
-		return -EINVAL;
+		goto out_free;
 	}
 
 	/* Check hooks all assigned */
@@ -678,17 +686,20 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
 		if (newinfo->hook_entry[i] == 0xFFFFFFFF) {
 			duprintf("Invalid hook entry %u %u\n",
 				 i, repl->hook_entry[i]);
-			return -EINVAL;
+			goto out_free;
 		}
 		if (newinfo->underflow[i] == 0xFFFFFFFF) {
 			duprintf("Invalid underflow %u %u\n",
 				 i, repl->underflow[i]);
-			return -EINVAL;
+			goto out_free;
 		}
 	}
 
-	if (!mark_source_chains(newinfo, repl->valid_hooks, entry0))
-		return -ELOOP;
+	if (!mark_source_chains(newinfo, repl->valid_hooks, entry0, offsets)) {
+		ret = -ELOOP;
+		goto out_free;
+	}
+	kvfree(offsets);
 
 	/* Finally, each sanity check must pass */
 	i = 0;
@@ -714,6 +725,9 @@ static int translate_table(struct xt_table_info *newinfo, void *entry0,
 			memcpy(newinfo->entries[i], entry0, newinfo->size);
 	}
 
+	return ret;
+ out_free:
+	kvfree(offsets);
 	return ret;
 }
 
