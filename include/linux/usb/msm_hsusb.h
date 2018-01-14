ec292d275127e05f6038d63b59d5decc89588f7b
wj <c.potato23@gmail.com>
2018-01-14 22:00:12 +0800

    msm_hsusb: Enable 2400mA charging current

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -108,7 +108,7 @@ enum msm_usb_phy_type {
 	QUSB_ULPI_PHY,
 };
 
-#define IDEV_CHG_MAX	2000
+#define IDEV_CHG_MAX	2400
 #define IUNIT		100
 #define IDEV_HVDCP_CHG_MAX	1800
 #define IDEV_CHG_MIN 500
